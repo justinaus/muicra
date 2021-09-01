d4481a5874b959819c744dff9db893dc61bf58b9
bonkook koo <bonkook.koo@sirloin.co.kr>
2021-09-01 10:47:37 +0900

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import PhotosPage from './pages/PhotosPage';
+import PostsPage from './pages/PostsPage';
+import { RoutePath } from './utils/enums';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path={RoutePath.Posts}>
+          <PostsPage />
+        </Route>
+        <Route path={RoutePath.Photos}>
+          <PhotosPage />
+        </Route>
+        <Redirect to={RoutePath.Posts} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/utils/enums.ts
@@ -0,0 +1,9 @@
+export enum RoutePath {
+  Photos = '/photos',
+  Posts = '/posts',
+}
+
+export enum ApiPath {
+  Photos = '/photos',
+  Posts = '/posts',
+}
