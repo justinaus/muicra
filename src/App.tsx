101e96c586cf3b8e5eb8b6ed209568ffa9a418d0
bonkook koo <bonkook.koo@sirloin.co.kr>
2021-09-01 11:26:48 +0900

    Add swr

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { SWRConfig } from 'swr';
 
 import Routes from './Routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json()),
+        onError: (error, key) => {
+          if (error.status !== 403 && error.status !== 404) {
+            // We can send the error to Sentry,
+            // or show a notification UI.
+            console.log('error!', key);
+          }
+        },
+      }}
+    >
+      <Routes />
+    </SWRConfig>
+  );
 }
 
 export default App;
--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import useSWR from 'swr';
+
+import { IPhoto } from '../interfaces/photo.interface';
 
 export default function PostsPage() {
-  return <div>PostsPage</div>;
+  const { data, error } = useSWR<IPhoto[]>(
+    'https://jsonplaceholder.typicode.com/photos?_page=1&_limit=10',
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  return <div>PostsPage {data.length}</div>;
 }
