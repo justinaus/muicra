77c38be9a656b0fc1e58b6ff3e2447671b0a27bc
justinaus <justinaus2k@gmail.com>
2021-09-02 10:39:28 +0900

    Add photos page - table

--- src/pages/PhotosPage.tsx
+++ src/pages/PhotosPage.tsx
@@ -1,5 +1,39 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
 import React from 'react';
 
+import usePhotos from '../hooks/usePhotos';
+
 export default function PhotosPage() {
-  return <div>PhotosPage</div>;
+  const { photos } = usePhotos();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">ID</TableCell>
+            <TableCell align="center">Album ID</TableCell>
+            <TableCell align="center">Title</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {photos.map((item) => (
+            <TableRow key={item.id} hover>
+              <TableCell align="center">{item.id}</TableCell>
+              <TableCell align="center">{item.albumId}</TableCell>
+              <TableCell align="center">{item.title}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
