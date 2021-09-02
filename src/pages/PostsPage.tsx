88c2807f6f7601646fc40ee0eb4ccc84a2a5cc50
justinaus <justinaus2k@gmail.com>
2021-09-02 10:20:03 +0900

    Add radio component

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,10 +1,29 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
+import { useCallback } from 'react';
+import { useState } from 'react';
 
+import { IRadio } from '../components/shared/radio/radio.interface';
+import RadioFormControl from '../components/shared/radio/RadioFormControl';
 import usePosts from '../hooks/usePosts';
 
+const RADIO_DATAS: IRadio[] = [
+  { id: 'albumId' },
+  { id: 'id' },
+  { id: 'title', disabled: true },
+];
+
 export default function PostsPage() {
+  const [currentId, setCurrentId] = useState('id');
+
   const { posts } = usePosts();
+
+  const handleChangeRadio = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>, value: string) => {
+      setCurrentId(value);
+    },
+    [],
+  );
 
   return (
     <div>
@@ -13,6 +32,12 @@ export default function PostsPage() {
         Hello
       </Button>
       <p className="text-lg font-semibold">122331</p>
+      <RadioFormControl
+        label="Test"
+        datas={RADIO_DATAS}
+        selectedId={currentId}
+        onChangeRadio={handleChangeRadio}
+      />
     </div>
   );
 }
